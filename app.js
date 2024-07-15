77c407415169a6a18f71dc9ee4f6de9da5bcca65
mohammedAmeeruddin <ameeruddin.codersnest@gmail.com>
2024-07-15 16:36:44 -0600

    added cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express =require("express");
 const app =express();
+const cors =require("cors");
 const userRouter =require("./src/router/userRouter");
 const productRouter =require("./src/router/productRouter");
 const orderidRouter =require("./src/router/orderidRouter");
+app.use(cors());
 app.use(express.json());
 
 app.use("/api",userRouter);
